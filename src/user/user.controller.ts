2d8e25deae37d76eddc7cb6c9bf3ed8343137a8a
alecastrovisk <alexandre.acrsf@gmail.com>
2023-03-13 10:47:29 -0300

    Revert "adapting project to microservices"
    This reverts commit d08b971be62c2e81e2402808c608dd3e063ebec4.
    
    "Reverting microservices"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,56 +1,56 @@
-import { Body, Controller, Get, OnModuleInit, Param, Post } from '@nestjs/common';
-import { Client, ClientKafka, Transport } from '@nestjs/microservices';
+import { Body, Controller, Delete, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Observable } from 'rxjs';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateUserDTO } from './dto/create-user.input';
+import { UpdateUserDTO } from './dto/update-user.input';
 import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @ApiTags('users')
 @Controller('user')
-export class UserController implements OnModuleInit {
-    @Client({
-        transport: Transport.KAFKA,
-        options: {
-            client: {
-                clientId: 'user',
-                brokers: ['localhost:9092']
-            },
-            consumer: {
-                groupId: 'user-consumer',
-                allowAutoTopicCreation: true
-            }
-        }
-    })
-
-    private client: ClientKafka;
-
-    async onModuleInit() {
-        const requestPatterns = [
-            'find-all-users',
-            'find-user',
-            'create-user'
-        ];
-
-        requestPatterns.forEach(async pattern => {
-            this.client.subscribeToResponseOf(pattern);
-            await this.client.connect()
-        })
-    }
+export class UserController {
+    constructor(
+        private userService: UserService
+    ) { }
 
     @Post()
-    create(@Body() data: CreateUserDTO): Observable<User> {
-        const user = this.client.send('create-user', data);
+    async create(@Body() data: CreateUserDTO): Promise<User> {
+        const user = await this.userService.createUser(data);
         return user;
     }
 
-    @Get()
-    findAll(): Observable<User[]> {
-        return this.client.send('find-all-users', {});
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Put(':id')
+    async update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() data: UpdateUserDTO
+    ): Promise<Object> {
+        const user = await this.userService.updateUser(id, data);
+        return user;
     }
 
-    @Get()
-    findOne(@Param('id') id: number): Observable<User> {
-        return this.client.send('find-user', { id });
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Delete(':id')
+    async delete(
+        @Param('id', ParseIntPipe) id: number
+    ): Promise<Object> {
+        await this.userService.deleteUser(id);
+        return {
+            message: 'Usuário deletado!'
+        };
     }
 
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Delete('soft/:id')
+    async softDelete(
+        @Param('id', ParseIntPipe) id: number
+    ): Promise<Object> {
+        await this.userService.softDeleteUser(id);
+        return {
+            message: 'Usuário deletado!'
+        }
+    }
 }
